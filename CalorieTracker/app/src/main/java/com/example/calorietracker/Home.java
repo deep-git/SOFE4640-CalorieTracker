eb1b1ffb017dea0ef1684093ef81813d11bab57c
deep-git <deepakthangella5@gmail.com>
2022-11-17 05:23:10 -0500

    Add designs and calendar selection

--- CalorieTracker/app/src/main/java/com/example/calorietracker/Home.java
+++ CalorieTracker/app/src/main/java/com/example/calorietracker/Home.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class Home extends AppCompatActivity {
 
     ImageView home;
     ImageView progress;
     ImageView profile;
+    TextView calendarSelection;
+    TextView date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,34 @@ public class Home extends AppCompatActivity {
         home = (ImageView) findViewById(R.id.home);
         progress = (ImageView) findViewById(R.id.progress);
         profile = (ImageView) findViewById(R.id.profile);
+        calendarSelection = (TextView) findViewById(R.id.calendarSelection);
+        date = (TextView) findViewById(R.id.date);
+
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.clear();
+
+        Long today = MaterialDatePicker.todayInUtcMilliseconds();
+
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Select a date");
+        builder.setSelection(today);
+        MaterialDatePicker<Long> materialDatePicker = builder.build();
+
+        calendarSelection.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+            }
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+
+            @Override
+            public void onPositiveButtonClick(Long selection) {
+                date.setText(materialDatePicker.getHeaderText());
+            }
+        });
 
         home.setOnClickListener(new View.OnClickListener() {
 
