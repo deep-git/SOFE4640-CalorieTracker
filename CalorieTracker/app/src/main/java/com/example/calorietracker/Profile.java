6c99e1b506b2324577a3e832d1237192f35925f5
Roniel Casaclang <rnlclng.pilar@gmail.com>
2022-11-19 15:36:02 -0500

    added username/email retrieval in profile page

--- CalorieTracker/app/src/main/java/com/example/calorietracker/Profile.java
+++ CalorieTracker/app/src/main/java/com/example/calorietracker/Profile.java
@@ -1,23 +1,43 @@
 package com.example.calorietracker;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class Profile extends AppCompatActivity {
 
     TextView logout;
+    TextView username;
+    TextView email;
+
     ImageView home;
     ImageView progress;
     ImageView profile;
+
     FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    FirebaseFirestore fdb;
+    FirebaseUser user;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +49,10 @@ public class Profile extends AppCompatActivity {
         progress = (ImageView) findViewById(R.id.progress);
         profile = (ImageView) findViewById(R.id.profile);
 
-        home.setOnClickListener(new View.OnClickListener() {
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
 
+        home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Profile.this, Home.class);
@@ -39,7 +61,6 @@ public class Profile extends AppCompatActivity {
         });
 
         profile.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Profile.this, Profile.class);
@@ -48,7 +69,6 @@ public class Profile extends AppCompatActivity {
         });
 
         logout.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
@@ -57,5 +77,43 @@ public class Profile extends AppCompatActivity {
                 finish();
             }
         });
+
+//  --------------------------------------------------------------------------------------------------
+
+        fdb = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        if (user != null) {
+            String emailDetail = user.getEmail();
+            email.setText(emailDetail);
+
+            fdb.collection("users")
+                    .document(userID)
+                    .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+                            String usernameDetail = document.getString("username");
+                            username.setText(usernameDetail);
+
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });
+
+        }else{
+            Toast.makeText(Profile.this, "Error: unable to retrieve profile details.", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
